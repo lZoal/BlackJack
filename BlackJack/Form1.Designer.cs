92bc2bb5a8048b209c7406907d4b106dd2b2fa2a
lzoal <dlaehdgnl02@gmail.com>
2022-11-24 13:09:09 +0900

    edit gitignore
    debug 추가

--- BlackJack/Form1.Designer.cs
+++ BlackJack/Form1.Designer.cs
@@ -35,9 +35,9 @@
             // pictureBox1
             // 
             this.pictureBox1.BackColor = System.Drawing.SystemColors.ButtonShadow;
-            this.pictureBox1.Location = new System.Drawing.Point(152, 54);
+            this.pictureBox1.Location = new System.Drawing.Point(12, 12);
             this.pictureBox1.Name = "pictureBox1";
-            this.pictureBox1.Size = new System.Drawing.Size(345, 190);
+            this.pictureBox1.Size = new System.Drawing.Size(135, 186);
             this.pictureBox1.TabIndex = 0;
             this.pictureBox1.TabStop = false;
             this.pictureBox1.Click += new System.EventHandler(this.pictureBox1_Click);
