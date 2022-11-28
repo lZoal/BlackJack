073df73665057dec80e913b426b52c165ad3eb8a
lzoal <dlaehdgnl02@gmail.com>
2022-11-28 21:11:22 +0900

    v0.0

--- BlackJack/Form1.Designer.cs
+++ BlackJack/Form1.Designer.cs
@@ -123,6 +123,7 @@
             this.button2.TabIndex = 3;
             this.button2.Text = "Stand";
             this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
             // 
             // user2
             // 
--- BlackJack/Form1.cs
+++ BlackJack/Form1.cs
@@ -26,27 +26,40 @@ namespace BlackJack
         {
 
             InitializeComponent();
-            c_user.Add(user0); c_user.Add(user1); c_user.Add(user2); c_user.Add(user3); c_user.Add(user4);c_user.Add(user5);
-            
+            c_user.Add(user0); c_user.Add(user1); c_user.Add(user2); c_user.Add(user3); c_user.Add(user4); c_user.Add(user5);
+            d_com.Add(dealer0); d_com.Add(dealer1); d_com.Add(dealer2); d_com.Add(dealer3); d_com.Add(dealer4); d_com.Add(dealer5); 
         }
 
-        private void Form1_Load(object sender, EventArgs e)
+        private void Form1_Load(object sender, EventArgs e) // 처음 로드 했을 때 카드 두장씩 불러오기
         {
-
-            user0.Load(card.deck[cnt].Second);
-            user1.Load(card.deck[cnt+1].Second);
-            uservalue = card.deck[cnt].First + card.deck[cnt + 1].First;
-            if ((card.deck[cnt].First==1 || card.deck[cnt+1].First==1))
+            c_user[0].Load(card.deck[cnt++].Second); //유저 0번째 1번째 불러오기
+            c_user[1].Load(card.deck[cnt++].Second);
+            d_com[0].Load("./img/52.png"); // 딜러패 첫장은 비공개
+            cnt++;
+            d_com[1].Load(card.deck[cnt++].Second);  //딜러패 두째장
+            uservalue = card.deck[0].First + card.deck[1].First; //유저패 총합
+            dealervalue = card.deck[2].First + card.deck[3].First; //딜러패 총합
+            
+            if ((card.deck[0].First==1 || card.deck[1].First==1) && uservalue<21) // A(에이스) 카드는 1 또는 11로 처리함 즉 value 값이 1 인경우 == A 인경우
             {
-                 textBox1.Text = (uservalue).ToString()+", " + (card.deck[cnt].First + card.deck[cnt + 1].First+10).ToString() ;
+                 textBox1.Text = (uservalue).ToString()+", " + (card.deck[0].First + card.deck[ 1].First+10).ToString() ;
             }
             else
-            textBox1.Text = (card.deck[cnt].First + card.deck[cnt + 1].First).ToString();
+            textBox1.Text = (card.deck[0].First + card.deck[1].First).ToString();
 
-            textBox2.Text = card.deck[cnt+=2].Second;
-            textBox3.Text = "남은 카드: " + (52 - cnt).ToString();
-
+            if ((card.deck[2].First == 1 || card.deck[3].First == 1) && uservalue < 21)
+            {
+                textBox4.Text = (uservalue).ToString() + ", " + (card.deck[0].First + card.deck[ 1].First + 10).ToString();
+            }
+            else
+                textBox4.Text = (card.deck[2].First + card.deck[3].First).ToString();
         }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            d_com[0].Load(card.deck[2].Second);
+        }
+
         int usercnt = 2;
         private void button1_Click(object sender, EventArgs e)
         {
@@ -57,7 +70,12 @@ namespace BlackJack
             if(21>=uservalue)
                 textBox1.Text = uservalue.ToString();
             else
-                textBox1.Text = uservalue.ToString() +" 21 초과로 패배";
+            {
+                textBox1.Text = uservalue.ToString() + " 21 초과로 패배";
+                button1.Enabled = false;
+            }
+
+
 
         }
     }
