6c9db5547cb8f303692ca14afa97dd87bce9738f
lzoal <dlaehdgnl02@gmail.com>
2022-11-28 20:31:06 +0900

    init 2

--- BlackJack/Form1.cs
+++ BlackJack/Form1.cs
@@ -21,8 +21,40 @@ namespace BlackJack
 
         private void pictureBox1_Click(object sender, EventArgs e)
         {
+<<<<<<< Updated upstream
 
             pictureBox1.Load(card.deck[0].Second);
+=======
+           for(int i=2;i<c_user.Count;i++)
+            {
+                c_user[i].BackColor = Color.Transparent;
+            }
+            user0.Load(card.deck[cnt].Second);
+            user1.Load(card.deck[cnt+1].Second);
+            uservalue = card.deck[cnt].First + card.deck[cnt + 1].First;
+            if ((card.deck[cnt].First==1 || card.deck[cnt+1].First==1))
+            {
+                 textBox1.Text = (uservalue).ToString()+", " + (card.deck[cnt].First + card.deck[cnt + 1].First+10).ToString() ;
+            }
+            else
+            textBox1.Text = (card.deck[cnt].First + card.deck[cnt + 1].First).ToString();
+
+            textBox2.Text = card.deck[cnt+=2].Second;
+            textBox3.Text = "남은 카드: " + (52 - cnt).ToString();
+
+        }
+        int usercnt = 2;
+        private void button1_Click(object sender, EventArgs e)
+        {
+            
+            c_user[usercnt++].Load(card.deck[++cnt].Second);
+            textBox3.Text = "남은 카드: " + (52 - cnt).ToString();
+            uservalue = uservalue + card.deck[cnt].First;
+            if(21>=uservalue)
+                textBox1.Text = uservalue.ToString();
+            else
+                textBox1.Text = uservalue.ToString() +" 21 초과로 패배";
+>>>>>>> Stashed changes
 
         }
     }
